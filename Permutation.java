183aa262767532b123aadd327b94a881801a91c9
Michael Liu <liux3604@gmail.com>
2017-08-11 00:35:24 -0500

    finished assignment

--- Permutation.java
+++ Permutation.java
@@ -1,9 +1,25 @@
-import edu.princeton.cs.algs4.*;
+import edu.princeton.cs.algs4.In;
+
+import java.util.Iterator;
 
 
 public class Permutation {
     public static void main(String[] args)
     {
+        int k = Integer.parseInt(args[0]);
+        String fileName = args[2];
+        In fileInput = new In(fileName);
+        RandomizedQueue<String> newQueue = new RandomizedQueue<>();
+        while (!fileInput.isEmpty())
+        {
+            newQueue.enqueue(fileInput.readString());
+        }
+        fileInput.close();
+
+        for (int i=0; i<k;i++)
+        {
+            System.out.print(newQueue.dequeue()+" ");
+        }
 
     }
 }
